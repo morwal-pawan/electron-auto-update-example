8deabb64254ac515b7b27b014facbd5612de4737
morwal-pawan <pawan.mahawar@unacademy.com>
2023-10-12 16:34:20 +0530

    fix require is not found

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false
     },
   });
   mainWindow.loadFile('index.html');
@@ -18,6 +19,8 @@ function createWindow () {
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
+  mainWindow.webContents.openDevTools();
+
 }
 
 app.on('ready', () => {
