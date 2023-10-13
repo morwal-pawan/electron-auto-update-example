2d2635be9cca668d65f08eeade75e7ef5ed1cc65
Pawan Mahawar <pawanmahavir350@gmail.com>
2023-10-13 11:29:42 +0530

    added the system tray

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain ,Menu, nativeImage, Tray} = require('electron');
 const { autoUpdater } = require('electron-updater');
+const path = require('path')
 
 let mainWindow;
+let tray = null
+
 autoUpdater.autoDownload=false;
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -16,16 +19,21 @@ function createWindow() {
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
+  // ipcMain.send('app_version', { version: app.getVersion() });
   mainWindow.once('ready-to-show', () => {
-    // autoUpdater.checkForUpdatesAndNotify();
-    autoUpdater.checkForUpdates();
+    console.log("ready-to-show")
+    // autoUpdater.checkForUpdates();
   });
   mainWindow.webContents.openDevTools();
 
 }
 
 app.on('ready', () => {
-  createWindow();
+  console.log("aap ready")
+  autoUpdater.checkForUpdates();
+  if(!tray){
+    createTray();
+  }
   const shouldStartOnLogin= true;
   app.setLoginItemSettings({
     openAtLogin:shouldStartOnLogin,
@@ -40,6 +48,7 @@ app.on('window-all-closed', function () {
 });
 
 app.on('activate', function () {
+  console.log("activate ",{mainWindow})
   if (mainWindow === null) {
     createWindow();
   }
@@ -47,20 +56,44 @@ app.on('activate', function () {
 
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
+  console.log("app_version",{  version: app.getVersion() })
 });
 
 autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+  // mainWindow.webContents.send('update_available');
+  console.log("update-available")
   autoUpdater.downloadUpdate();
 });
 
 autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+  // mainWindow.webContents.send('update_downloaded');
+  console.log("update-downloaded")
   setTimeout(()=>{
     autoUpdater.quitAndInstall();
   },2000)
 });
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
+
+function createTray () {
+  const icon = path.join(__dirname, '/app.png');
+  const trayicon = nativeImage.createFromPath(icon)
+  tray = new Tray(trayicon.resize({ width: 16 }))
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        createWindow()
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.quit() // actually quit the app.
+      }
+    },
+  ])
+
+  tray.setContextMenu(contextMenu)
+}
+
+  
