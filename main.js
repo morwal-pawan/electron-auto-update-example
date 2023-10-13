c0cf701f3503fe552ad6e02ebddc0780efdc86a4
Pawan Mahawar <pawanmahavir350@gmail.com>
2023-10-13 15:17:44 +0530

    only one instance will be created

--- main.js
+++ main.js
@@ -1,111 +1,125 @@
-const { app, BrowserWindow, ipcMain ,Menu, nativeImage, Tray} = require('electron');
-const { autoUpdater } = require('electron-updater');
-const path = require('path')
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Menu,
+  nativeImage,
+  Tray,
+} = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
 
 let mainWindow;
-let tray = null
-let quitApp=false;
-autoUpdater.autoDownload=false;
-function createWindow() { 
+let tray = null;
+let quitApp = false;
+autoUpdater.autoDownload = false;
+
+const gotTheLock = app.requestSingleInstanceLock();
+console.log("gotTheLock", { gotTheLock });
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on("second-instance", () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
     },
   });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
- 
-  
-  mainWindow.once('ready-to-show', () => {
-    console.log("ready-to-show")
+
+  mainWindow.once("ready-to-show", () => {
+    console.log("ready-to-show");
     // autoUpdater.checkForUpdates();
   });
-  mainWindow.on('close', function (evt) {
-    console.log("mainWindow close",{quitApp})
-    if(!quitApp){
+  mainWindow.on("close", function (evt) {
+    console.log("mainWindow close", { quitApp });
+    if (!quitApp) {
       evt.preventDefault();
-      mainWindow.hide()
+      mainWindow.hide();
     }
-    
-});
+  });
   // mainWindow.webContents.openDevTools();
-
 }
 
-app.on('ready', () => {
-  console.log("aap ready")
+app.on("ready", () => {
+  console.log("aap ready");
   autoUpdater.checkForUpdates();
-  if(!tray){
+  if (!tray) {
     createTray();
   }
-  const shouldStartOnLogin= true;
+  const shouldStartOnLogin = true;
   app.setLoginItemSettings({
-    openAtLogin:shouldStartOnLogin,
-    openAsHidden:false
-  })
+    openAtLogin: shouldStartOnLogin,
+    openAsHidden: false,
+  });
 });
 
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
-  console.log("activate ",{mainWindow})
+app.on("activate", function () {
+  console.log("activate ", { mainWindow });
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-  console.log("app_version",{  version: app.getVersion() })
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+  console.log("app_version", { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
+autoUpdater.on("update-available", () => {
   // mainWindow.webContents.send('update_available');
-  console.log("update-available")
+  console.log("update-available");
   autoUpdater.downloadUpdate();
 });
 
-autoUpdater.on('update-downloaded', () => {
+autoUpdater.on("update-downloaded", () => {
   // mainWindow.webContents.send('update_downloaded');
-  console.log("update-downloaded")
-  setTimeout(()=>{
+  console.log("update-downloaded");
+  setTimeout(() => {
     quitApp = true;
     autoUpdater.quitAndInstall();
-  },8000)
+  }, 8000);
 });
 
-
-function createTray () {
-  const icon = path.join(__dirname, '/app.png');
-  const trayicon = nativeImage.createFromPath(icon)
-  tray = new Tray(trayicon.resize({ width: 16 }))
+function createTray() {
+  const icon = path.join(__dirname, "/app.png");
+  const trayicon = nativeImage.createFromPath(icon);
+  tray = new Tray(trayicon.resize({ width: 16 }));
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Show App',
+      label: "Show App",
       click: () => {
-        createWindow()
-      }
+        createWindow();
+      },
     },
     {
-      label: 'Quit',
+      label: "Quit",
       click: () => {
-        quitApp=true;
-        app.quit() // actually quit the app.
-      }
+        quitApp = true;
+        app.quit(); // actually quit the app.
+      },
     },
-  ])
+  ]);
 
-  tray.setContextMenu(contextMenu)
+  tray.setContextMenu(contextMenu);
 }
-
-  
